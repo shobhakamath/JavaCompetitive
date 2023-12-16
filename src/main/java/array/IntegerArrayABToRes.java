30ff5ef0d5befe5cacd05bfb0bf3b03d5d2a5901
shobhakamath <shobha.s.kamath@gmail.com>
2023-12-16 17:43:51 +0400

    algorithms practiced today

---
+++ src/main/java/array/IntegerArrayABToRes.java
@@ -0,0 +1,57 @@
+package array;
+
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.TreeMap;
+
+
+/*
+Given 2 int arrays a[]={1,2,3,4,5} and b[]={0,1,2,2,3}
+where b is the array of index
+where a.length=b.length
+
+We need to create a new array res where res[b[i]]= a[i]
+where if res[b[i]] contains an element already,we need to move it to the right
+
+In the above example
+res = {1,2,4,5,3}
+ */
+public class IntegerArrayABToRes {
+
+    public static void main(String[] args) {
+        int a[] = {1, 2, 3, 4, 5};
+        int b[] = {0, 1, 2, 2, 2};
+
+        Arrays.stream(new IntegerArrayABToRes().replaceAiWithBi(a, b))
+                .forEach(System.out::print);
+
+        b = new int[]{0, 1, 2, 2, 3};
+        Arrays.stream(new IntegerArrayABToRes().replaceAiWithBi(a, b))
+                .forEach(System.out::print);
+    }
+
+    private int[] replaceAiWithBi(int[] a, int[] b) {
+        Map<Integer, Integer> mapInteger = new TreeMap<>();
+        for (int i = 0; i < a.length; i++) {
+            if (mapInteger.get(b[i]) != null) {
+                reverseMap(mapInteger, b[i]);
+            }
+            mapInteger.put(b[i], a[i]);
+        }
+        return mapInteger.values().stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
+    }
+
+    private void reverseMap(Map<Integer, Integer> mapInteger, int i) {
+        if (mapInteger.get(i) == null) {
+            mapInteger.put(i, mapInteger.get(i - 1));
+            return;
+        }
+        reverseMap(mapInteger, i + 1);
+        mapInteger.put(i, mapInteger.get(i - 1));
+    }
+}
+
+
---
+++ src/main/java/array/LongestLengthOfCharacterInAString.java
@@ -0,0 +1,44 @@
+package array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+Given a string "abcdefabcdah"
+The longest length as per the program should be 11
+as a's first index =0 and last index is 10 , hence length is 11
+ */
+public class LongestLengthOfCharacterInAString {
+
+
+    public static void main(String[] args) {
+        System.out.println(findLongestLength("abcdefabcdah"));//11
+
+        System.out.println(findLongestLength("abcdefcdh"));//5 as c is of length 5
+    }
+
+    private static int findLongestLength(String str) {
+        Map<Character, Index> characterIndex = new HashMap<>();
+        int maxLength = 0;
+        int i = 0;
+        for (char ch : str.toCharArray()) {
+            Index idx = characterIndex.getOrDefault(ch, new Index(i, i));
+            idx.lastIndex=i;
+            maxLength = Math.max(maxLength, idx.lastIndex - idx.firstIndex + 1);
+            characterIndex.put(ch,idx);
+            i++;
+        }
+        return maxLength;
+    }
+}
+
+
+class Index {
+    int firstIndex = -1;
+    int lastIndex = -1;
+
+    public Index(int i1, int i2) {
+        firstIndex = i1;
+        lastIndex = i2;
+    }
+}
